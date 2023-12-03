6d407569104d752fd93feb72c80fae744b804cab
Luka-DV <luka.guva@gmail.com>
2023-12-03 12:55:58 +0100

    add solution to task 1 of day 1

--- 2023/_Day1/js/main.js
+++ 2023/_Day1/js/main.js
@@ -1,10 +1,8 @@
 
 const file = document.querySelector("input");
 
-let textOutput;
-let textArray = null;
 
-file.addEventListener("change", () => {
+/* file.addEventListener("change", () => {
 
   const fileReader = new FileReader();
 
@@ -12,14 +10,57 @@ file.addEventListener("change", () => {
 
   fileReader.addEventListener("load", () => {
 
-    textOutput = fileReader.result;
-    textArray = textOutput.split("\n");
-   
+    const textOutput = fileReader.result;
+    const textArray = textOutput.split("\n");
+
+    console.log(findCoordinates(textArray));
   })
+}); */
+
+file.addEventListener("change", () => {
+
+  const fileReader = new FileReader();
+
+  fileReader.readAsText(file.files[0]);
+
+  fileReader.onload = function() {
+
+    const textOutput = fileReader.result;
+    const textArray = textOutput.split("\n");
+
+    console.log(findCoordinates(textArray));
+  };
+
+  fileReader.onerror = function() {
+    console.log(reader.error);
+  }
 });
 
 
-function findCoordinates() {
 
+function findCoordinates(array) {
+
+  let sumOfCalibrationValues = 0;
+
+  for(let item of array) {
+
+    let calibrationValue = "";
+
+    for(let i = 0; i < item.length; i++) {
+      if(!isNaN(item[i])) {
+        calibrationValue += item[i];
+        break;
+      }
+    }
+    for(let i = item.length - 1; i >= 0; i--) {
+      if(!isNaN(item[i]) && item[i] !== "\r") {
+        calibrationValue += item[i];
+        break;
+      }
+    }
+    sumOfCalibrationValues += Number(calibrationValue);
+  }
+  return sumOfCalibrationValues;
 }
 
+//the result for day 1 task 1 is: 55090
