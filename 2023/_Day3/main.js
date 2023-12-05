976771d9a08f5576f1c0916b62514a029a97e2c5
Luka-DV <luka.guva@gmail.com>
2023-12-05 15:32:41 +0100

    add Day 3 task 2 solution

--- 2023/_Day3/main.js
+++ 2023/_Day3/main.js
@@ -15,87 +15,177 @@ file.addEventListener("change", () => {
     const textOutput = fileReader.result;
     const textArray = textOutput.split("\n");
 
-    findNums(textArray);
+    const mapCoordinates = findNums(textArray);
+    findGears(textArray, mapCoordinates);
 
     }
   });
 
 
-  //Task1:
+//Task1: ++++
 
 
 
-  function findNums(array) {
+function findNums(array) {
 
-    let sumOfParts = 0;
+  let sumOfParts = 0;
 
-    for(let i = 0; i < array.length; i++) {
-      
-      for(let j = 0; j < array[i].length; j++) {
+  const allNumsMap = new Map(); 
 
-        let correctNums = [];
+  for(let i = 0; i < array.length; i++) {
+    
+    for(let j = 0; j < array[i].length; j++) {
 
-        if(!isNaN(array[i][j])) {
-          correctNums.push(j)
+      let correctNums = [];
 
-          let k = j;
+      if(!isNaN(array[i][j])) {
+        correctNums.push(j)
 
-          while(!isNaN(array[i][k+1])) {
-            k++;
+        let k = j;
+
+        while(!isNaN(array[i][k+1])) {
+          k++;
+        }
+
+        correctNums.push(k)
+
+        j = k;
+
+        const result = checkIfGoodNum(correctNums, i, array);
+
+        if(result) {
+
+          let num = "";
+
+          for(let m = correctNums[0]; m <= correctNums[1]; m++) {
+            num += array[i][m]
           }
 
-          correctNums.push(k)
+          // task 2 //
 
-          j = k;
-
-          const result = checkIfGoodNum(correctNums, i, array);
-
-          if(result) {
-
-            let num = "";
-
-            for(let m = correctNums[0]; m <= correctNums[1]; m++) {
-              num += array[i][m]
-            }
-
-            sumOfParts += Number(num);
-
+          for(let m = correctNums[0]; m <= correctNums[1]; m++) {
+            allNumsMap.set(`${[i , m]}`, num);
           }
-       }
+
+          ///////////
+
+          sumOfParts += Number(num);
+
+        }
       }
     }
+  }
 
-    console.log(sumOfParts);
+    console.log("Sum Of Parts: ", sumOfParts);
+    return allNumsMap;
   }
 
 
-  function checkIfGoodNum(arrayOfTwo, i, array) {
+function checkIfGoodNum(arrayOfTwo, i, array) {
 
-    let isCorrectNum = false;
+  let isCorrectNum = false;
 
-    if((array[i][arrayOfTwo[0] - 1] && array[i][arrayOfTwo[0] - 1] !== ".") || ( array[i][arrayOfTwo[1] + 1] && array[i][arrayOfTwo[1] + 1] !== ".")) {
-      isCorrectNum = true;
-      return isCorrectNum;
-    }
-
-    if(i !== 0) {
-      for(let n = arrayOfTwo[0] - 1; n <= arrayOfTwo[1] + 1; n++) {
-        if(array[i-1][n] && array[i-1][n] !== "." && isNaN(array[i-1][n])) {
-        isCorrectNum = true;
-        return isCorrectNum;
-        }
-      }
-    }
-
-    if(i !== array.length -1) {
-      for(let n = arrayOfTwo[0] - 1; n <= arrayOfTwo[1] + 1; n++) {
-        if(array[i+1][n] && (array[i+1][n] !== ".") && isNaN(array[i+1][n])) {
-        isCorrectNum = true;
-        return isCorrectNum;
-        }
-      }
-    }
-
+  if((array[i][arrayOfTwo[0] - 1] && array[i][arrayOfTwo[0] - 1] !== ".") || ( array[i][arrayOfTwo[1] + 1] && array[i][arrayOfTwo[1] + 1] !== ".")) {
+    isCorrectNum = true;
     return isCorrectNum;
   }
+
+  if(i !== 0) {
+    for(let n = arrayOfTwo[0] - 1; n <= arrayOfTwo[1] + 1; n++) {
+      if(array[i-1][n] && array[i-1][n] !== "." && isNaN(array[i-1][n])) {
+      isCorrectNum = true;
+      return isCorrectNum;
+      }
+    }
+  }
+
+  if(i !== array.length -1) {
+    for(let n = arrayOfTwo[0] - 1; n <= arrayOfTwo[1] + 1; n++) {
+      if(array[i+1][n] && (array[i+1][n] !== ".") && isNaN(array[i+1][n])) {
+      isCorrectNum = true;
+      return isCorrectNum;
+      }
+    }
+  }
+
+  return isCorrectNum;
+}
+
+
+  //task 2 +++
    
+  
+function findGears(array, mapCoordinates) {
+
+  let sumOfGears = 0;
+
+  for(let i = 0; i < array.length; i++) {
+    
+    for(let j = 0; j < array[i].length; j++) {
+
+      if(array[i][j] === "*") {
+
+        const numberCoordinates = checkIfGoodGear(j, i, array);
+
+        if(numberCoordinates) {
+          let number = 1;
+
+          for(let coordinates of numberCoordinates) {
+            number *= Number(mapCoordinates.get(coordinates));
+          }
+          sumOfGears += number;
+        }
+      }
+    }
+  }
+
+  console.log("sumOfGears: ", sumOfGears);
+}
+
+
+function checkIfGoodGear(j, i, array) {
+
+  let gearPoints = 0;
+
+  let coordinatesOfNumbersBesideGear = [];
+
+  if(!isNaN(array[i][j - 1])) {
+    gearPoints++;
+    coordinatesOfNumbersBesideGear.push(`${[i , j - 1]}`);
+  }
+
+  if(!isNaN(array[i][j + 1])) {
+    gearPoints++;
+    coordinatesOfNumbersBesideGear.push(`${[i , j + 1]}`);
+  }
+
+  if(i !== 0) {
+    for(let n = j - 1; n <= j + 1; n++) {
+      if(!isNaN(array[i-1][n])) {
+        gearPoints++;
+        coordinatesOfNumbersBesideGear.push(`${[i-1 , n]}`);
+        if(!isNaN(array[i-1][n+1])){
+          break;
+        }
+      }
+    }
+  }
+
+  if(i !== array.length -1) {
+    for(let n = j - 1; n <= j + 1; n++) {
+      if(!isNaN(array[i+1][n])) {
+        gearPoints++;
+        coordinatesOfNumbersBesideGear.push(`${[i+1 , n]}`);
+        if(!isNaN(array[i+1][n+1])){
+          break;
+        }
+      }
+    }
+  }
+
+  if(gearPoints === 2) {
+    return coordinatesOfNumbersBesideGear;
+  } else {
+    return null;
+  }
+}
